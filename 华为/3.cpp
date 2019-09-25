cee86bb485c706d577f05ef944a91cb9edfc350f
zhaobin <18260068839@163.com>
2019-09-25 20:18:53 +0800

    new file:   3.cpp 先排序后删除，unique返回的是去重最后一个字符`

---
+++ 华为/3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+    int n,temp;
+    
+    
+    while(cin>>n)
+    {
+        vector<int> inputArray;
+    for(int i=0;i<n;i++)
+    {
+        cin>>temp;
+        inputArray.push_back(temp);
+    }
+    sort(inputArray.begin(),inputArray.end());   
+    inputArray.erase(unique(inputArray.begin(), inputArray.end()), inputArray.end());
+    for(int i=0;i<inputArray.size();i++)
+        cout<<inputArray[i]<<endl;
+   }
+    return 0;
+}
